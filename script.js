dde884d711b2aaefd03a3809ed13e432f36339fe
skynjdjo <51291085+skynjdjo@users.noreply.github.com>
2019-06-02 21:32:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,323 @@
+//3 Scripts: SMOOTH SCROLL, GOOGLE MAPS, CHANGE CLASS
+
+//SMOOTH SCROLLING
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+//CHANGE CSS CLASS IN HTML
+	$(document).scroll(function() {
+	var swap_class = document.getElementById("change_class");
+
+	   if($(window).scrollTop() === 0) {
+	     swap_class.classList.remove("menu_color_change");
+	   } else {
+	   		swap_class.classList.add("menu_color_change");
+	   	}
+	});
+
+//GOOGLE MAPS
+        function initMap() {
+        var MargaretRiver = {lat: -33.9536, lng: 115.07391};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: MargaretRiver,
+          styles: [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ebe3cd"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#523735"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f1e6"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#c9b2a6"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#dcd2be"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#ae9e90"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#93817c"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#a5b076"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#447530"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f1e6"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#fdfcf8"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f8c967"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#e9bc62"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e98d58"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#db8555"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#806b63"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8f7d77"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "labels.text.stroke",
+    "stylers": [ 
+      {
+        "color": "#ebe3cd"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#b9d3c2"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#92998d"
+      }
+    ]
+  }
+]
+        });
+        var marker = new google.maps.Marker({
+          position: MargaretRiver,
+          map: map
+        });
+      }
+
+	  function sentOrder(){
+		var order_name = document.getElementById("sent_name");
+		var order_unit = document.getElementById("sent_unit");
+		var order_amount = document.getElementById("sent_amount");
+		var listbar = document.getElementById("prod_select");
+		var Today=new Date();
+		if(order_name.value != '' && order_amount.value != '' && order_unit.value != ''){
+		var ref_1 = firebase.database().ref("PB/"+listbar.value);
+			ref_1.once('value').then(function(snapshot) {
+				var db = firebase.firestore();
+				db.collection("訂單").doc(order_name.value + 'From' + "=web=" + '@' + Today.getFullYear() + (Today.getMonth()+1) + Today.getDate() + Today.getHours() + Today.getMinutes() + Today.getSeconds()).set({
+					購買人: order_name.value,
+					產品: listbar.options.namedItem(listbar.value).text,
+					數量: order_amount.value,
+					產品代號: listbar.value,
+					購買單位: order_unit.value,
+					金額:(snapshot.val().price * order_amount.value) 
+				})
+				.then(function() {
+				console.log("Document successfully written!");
+					alert('成功送出');
+					order_name.value = '';
+					order_unit.value = '';
+					order_amount.value = '';
+				})
+				.catch(function(error) {
+					console.error("Error writing document: ", error);
+					alert('Error writing document');
+				});
+			});
+		}else{
+			alert('缺少資料');
+		}
+	  }
+	
+
+	  function loadpit(a){
+	   
+		var elmnt = document.getElementById("order_page");
+		elmnt.scrollIntoView(true);
+		var listbar = document.getElementById("prod_select");
+		listbar.value = a;
+		//document.getElementById("prod1").style.backgroundImage = "url(./images/peach.jpg)";
+	  }
